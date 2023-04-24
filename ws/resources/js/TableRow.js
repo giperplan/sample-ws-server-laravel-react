e20986d6c0b340e620217a43faf1ff98c7976afd
Yurii Kliushnykov <yurii.kliushnykov@auslogics.com>
2023-04-24 20:00:15 +0400

    fix date

--- ws/resources/js/TableRow.js
+++ ws/resources/js/TableRow.js
@@ -3,13 +3,32 @@ import React from 'react';
 function TableRow(props) {
     const { item, columns } = props;
 
+    function ts2date(timestamp) {
+        const now = new Date(timestamp * 1000);
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        return `${hours}:${minutes}:${seconds} ${day}.${month}.${year}`;
+    }
+
     return (
         <tr className={item.highlight ? 'highlight' : ''}>
-            {columns.map(column => (
-                <td key={column}>
-                    {item[column]}
-                </td>
-            ))}
+            {columns.map(column => {
+                let value = '';
+                if (column === 'date') {
+                    value = ts2date(item[column]);
+                } else {
+                    value = item[column];
+                }
+                return (
+                    <td key={column}>
+                        {value}
+                    </td>
+                )
+            })}
         </tr>
     );
 }
